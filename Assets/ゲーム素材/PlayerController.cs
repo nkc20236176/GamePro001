ba3b6467fce2da2cebfd061e9cd9d8ee8cab0e79
nkc20236176 <nkc23.denpa.sugiura.masaya@gmail.com>
2023-05-25 10:39:19 +0900

    Unityプロジェクトを追加

---
+++ Assets/ゲーム素材/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if(Input.GetKey (KeyCode.RightArrow))
+        {
+
+
+            transform.Translate(0, 0.03f, 0);
+           
+          
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(0, -0.03f, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(-0.03f, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0.03f, 0, 0);
+        }
+    }
+}
